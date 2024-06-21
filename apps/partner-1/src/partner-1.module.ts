abdacd790ed7f7bf8699f0c80d652f738c134238
Michael Nascimento <michael.nsc@outlook.com>
2024-06-21 00:03:54 +0000

    add guard

--- apps/partner-1/src/partner-1.module.ts
+++ apps/partner-1/src/partner-1.module.ts
@@ -3,11 +3,13 @@ import { EventsModule } from './events/events.module';
 import { SpotsModule } from './spots/spots.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '@app/core/prisma/prisma.module';
+import { AuthModule } from '@app/core';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: '.env.partner-1' }),
+    ConfigModule.forRoot({ envFilePath: '.env.partner-1', isGlobal: true }),
     PrismaModule,
+    AuthModule,
     EventsModule,
     SpotsModule,
   ],
--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -2,3 +2,5 @@ export * from './events/events-core.module';
 export * from './events/events.service';
 export * from './spots/spots-core.module';
 export * from './spots/spots.service';
+export * from './auth/auth.module';
+export * from './auth/auth.guard';
